60f60cb59d539b5424176d66c13410007452c3a0
Administrator <464834357@qq.com>
2023-03-22 17:26:10 +0800

    tag

---
+++ src/main/java/com/blog/blogapidemo/Controller/TagsController.java
@@ -0,0 +1,30 @@
+package com.blog.blogapidemo.Controller;
+
+
+import com.blog.blogapidemo.Service.TagService;
+import com.blog.blogapidemo.Vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tags")
+public class TagsController {
+
+    //tags/hot
+    @Autowired
+    TagService tagService;
+
+    @GetMapping("hot")
+public Result hot()
+    {
+        int limit=6;
+        return tagService.hots(6);
+
+    }
+
+
+
+
+}
